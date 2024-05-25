3647a3fbd1d249baf3e1bcc0c2b37cd412adf31d
GuardianAngel2138 <96695654+GuardianAngel2138@users.noreply.github.com>
2024-05-26 00:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def restrict_user_permissions(chat_id):
             'can_send_other_messages': False,
             'can_add_web_page_previews': False,
             'can_change_info': False,
-            'can_invite_users': False,
+            'can_invite_users': True,
             'can_pin_messages': False
         }
     }
@@ -98,13 +98,13 @@ def restore_user_permissions(chat_id):
         'chat_id': chat_id,
         'permissions': {
             'can_send_messages': True,
-            'can_send_media_messages': True,
-            'can_send_polls': True,
-            'can_send_other_messages': True,
-            'can_add_web_page_previews': True,
-            'can_change_info': True,
+            'can_send_media_messages': False,
+            'can_send_polls': False,
+            'can_send_other_messages': False,
+            'can_add_web_page_previews': False,
+            'can_change_info': False,
             'can_invite_users': True,
-            'can_pin_messages': True
+            'can_pin_messages': False
         }
     }
     requests.post(url, json=payload)
